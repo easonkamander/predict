af39c623d660349935334029e9d2d79ffb3bacf1
Eason Kamander <eason@kamander.com>
2020-03-22 16:46:28 -0400

    Lower Epoch and add test

--- analysis/train.py
+++ analysis/train.py
@@ -1,22 +1,13 @@
 import numpy as np
 import load
-
 import tensorflow as tf
 
-# from keras.models import Sequential
-# from keras.layers import Dense, Dropout, LSTM, Flatten, Activation
-# from keras.optimizers import Adam
-
-print('Loading...', end='')
-
-TESTS = 2
+TESTS = 3
 
 trainX = load.getBatchesX()[:-TESTS]
 trainY = load.getBatchesY()[:-TESTS]
 checkX = load.getBatchesX()[-TESTS:]
 checkY = load.getBatchesY()[-TESTS:]
-
-print('\rLoading...Done')
 
 # model = Sequential()
 # model.add(LSTM(load.MAX_CHOICES*8, input_shape=(load.TIMESTEPS, load.FEATURES), return_sequences=True))
@@ -26,8 +17,6 @@ print('\rLoading...Done')
 # model.add(Dense(16))
 # model.add(Dropout(0.2))
 # model.add(Dense(load.MAX_CHOICES, activation='sigmoid'))
-
-print(trainY)
 
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.LSTM(20, input_shape=(load.MAX_QUESTIONS, load.TOTAL_FEATURES)))
@@ -43,4 +32,6 @@ print(model.summary())
 
 model.compile(loss='mse', optimizer='adam')
 
-model.fit(trainX, trainY, epochs=200, validation_data=(checkX, checkY), shuffle=True)
+model.fit(trainX, trainY, epochs=2, validation_data=(checkX, checkY), shuffle=True)
+
+print(model.predict(checkX[-1]))
