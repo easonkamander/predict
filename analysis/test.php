bcf553c47dbe9cdc533e501967505056316aa719
Eason Kamander <eason@kamander.com>
2020-03-22 20:03:13 -0400

    Test 6

--- analysis/test.php
+++ analysis/test.php
@@ -1,20 +1,30 @@
 <?php
-$request = xmlrpc_encode_request('method', array(3, 4, 5));
-$url = 'http://localhost:8000/';
+// $request = xmlrpc_encode_request('method', array(3, 4, 5));
+// $url = 'http://localhost:8000/';
 
-$header[] = 'Content-type: text/xml';
-$header[] = 'Content-length: '.strlen($request);
+// $header[] = 'Content-type: text/xml';
+// $header[] = 'Content-length: '.strlen($request);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_TIMEOUT, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
-$data = curl_exec($ch);
-if (curl_errno($ch)) {
-	print curl_error($ch);
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, $url);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+// $data = curl_exec($ch);
+
+// echo $data;
+
+$request = xmlrpc_encode_request("method", array(1, 2, 3));
+$context = stream_context_create(array('http' => array(
+    'method' => "POST",
+    'header' => "Content-Type: text/xml",
+    'content' => $request
+)));
+$file = file_get_contents("http://localhost:8000/", false, $context);
+$response = xmlrpc_decode($file);
+if ($response && xmlrpc_is_fault($response)) {
+    trigger_error("xmlrpc: $response[faultString] ($response[faultCode])");
 } else {
-	curl_close($ch);
-	echo $data;
+    print_r($response);
 }
