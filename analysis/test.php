e601d3196b4c112a00776b6e18fd4f7a0dad5ee8
Eason Kamander <eason@kamander.com>
2020-03-23 21:57:05 -0400

    T

--- analysis/test.php
+++ analysis/test.php
@@ -1,30 +1,12 @@
 <?php
-// $request = xmlrpc_encode_request('method', array(3, 4, 5));
-// $url = 'http://localhost:8000/';
-
-// $header[] = 'Content-type: text/xml';
-// $header[] = 'Content-length: '.strlen($request);
-
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_URL, $url);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// curl_setopt($ch, CURLOPT_TIMEOUT, 1);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
-// $data = curl_exec($ch);
-
-// echo $data;
-
 $request = xmlrpc_encode_request("method", array(1, 2, 3));
+
 $context = stream_context_create(array('http' => array(
     'method' => "POST",
     'header' => "Content-Type: text/xml",
     'content' => $request
 )));
+
 $file = file_get_contents("http://localhost:8000/", false, $context);
-$response = xmlrpc_decode($file);
-if ($response && xmlrpc_is_fault($response)) {
-    trigger_error("xmlrpc: $response[faultString] ($response[faultCode])");
-} else {
-    print_r($response);
-}
+
+// $response = xmlrpc_decode($file);
