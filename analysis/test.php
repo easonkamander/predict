9df996d5a5b9debc3c60e15acd22973e1135d409
Eason Kamander <eason@kamander.com>
2020-03-23 21:59:11 -0400

    T2

--- analysis/test.php
+++ analysis/test.php
@@ -1,12 +1,6 @@
 <?php
-$request = xmlrpc_encode_request("method", array(1, 2, 3));
-
-$context = stream_context_create(array('http' => array(
-    'method' => "POST",
-    'header' => "Content-Type: text/xml",
-    'content' => $request
-)));
-
-$file = file_get_contents("http://localhost:8000/", false, $context);
-
-// $response = xmlrpc_decode($file);
+file_get_contents('http://localhost:8000/', false, stream_context_create(array('http' => array(
+    'method' => 'POST',
+    'header' => 'Content-Type: text/xml',
+    'content' => xmlrpc_encode_request('method', array(1, 2, 3))
+))));
