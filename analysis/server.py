01a021e93480f814882d06778972dc3ae8a49ccf
Eason Kamander <eason@kamander.com>
2020-03-24 19:56:13 -0400

    Show Prediction 2

--- analysis/server.py
+++ analysis/server.py
@@ -8,12 +8,13 @@ model = tf.keras.models.load_model('model.h5')
 
 with SimpleXMLRPCServer(('localhost', 8000)) as server:
 	def analyze (setID, batchFill):
-		print('abcd')
 		load.refresh()
 		batchX = load.getBatchX(setID, batchFill)
 		if batchX is not None:
 			batchY = model.predict(batchX)
 			load.setBatchY(setID, batchFill, batchY)
+		else:
+			print('It didnt find the batch')
 	server.register_function(analyze, 'analyze')
 
 	server.serve_forever()
