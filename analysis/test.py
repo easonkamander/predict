32709fb8207a35f360e9d2a7b49683f4f582dfab
Eason Kamander <eason@kamander.com>
2020-03-25 15:15:32 -0400

    Debug Async 2

--- analysis/test.py
+++ analysis/test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import threading
 import load
+import time
 import tensorflow as tf
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
@@ -8,6 +9,7 @@ from xmlrpc.server import SimpleXMLRPCRequestHandler
 model = tf.keras.models.load_model('model.h5')
 
 def analysisProcess (setID, batchFill):
+	time.sleep(3)
 	batchX = load.getBatchX(setID, batchFill)
 	if batchX is not None:
 		batchY = model.predict(batchX)[0]
