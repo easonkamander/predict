9497068ec8b31736429b33c194e05d866d86bc5f
Eason Kamander <eason@kamander.com>
2020-04-05 21:28:07 -0400

    Defer Cache 3

--- includes/page.php
+++ includes/page.php
@@ -4,7 +4,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="UTF-8">
 		<title><?php echo $pageDisplay; ?> - Predict</title>
-		<link rel="stylesheet" href="lib/mdc.min.css" defer>
+		<link rel="stylesheet" href="lib/mdc.min.css" defer async>
 		<style>
 			body {
 				margin: 0;
